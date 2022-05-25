aab8904948627bb8c88c46451cabbfb8d9710588
kamiy2743 <kamiy2743@gmail.com>
2022-05-25 11:30:26 +0900

    最高高度の記録ステート、PlayDataがグローバル変数みたいになってる

---
+++ Assets/MyAssets/PlayScreen/Scripts/PlayData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MT.Blocks;
+
+namespace MT.PlayScreen
+{
+    public class PlayData : MonoBehaviour
+    {
+        // グローバル変数じゃないか
+        public MaxHeight MaxHeight;
+    }
+}
---
+++ Assets/MyAssets/PlayScreen/Scripts/RecordingMaxHeightState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MT.Util;
+using MT.Blocks;
+
+namespace MT.PlayScreen
+{
+    public class RecordingMaxHeightState : MonoBehaviour, IState
+    {
+        [SerializeField] private PlayData _playData;
+        [SerializeField] private BlockStore _blockStore;
+        [SerializeField] private GameObject _nextStateObject;
+
+        private IState _nextState;
+
+        void Awake()
+        {
+            _nextState = _nextStateObject.GetComponent<IState>();
+            gameObject.SetActive(false);
+        }
+
+        public void Enter()
+        {
+            gameObject.SetActive(true);
+            _playData.MaxHeight = _blockStore.CalcMaxHeight();
+            ToNext();
+        }
+
+        public void ToNext()
+        {
+            gameObject.SetActive(false);
+            _nextState.Enter();
+        }
+    }
+}
+
