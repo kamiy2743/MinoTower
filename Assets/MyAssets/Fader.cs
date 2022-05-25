40ffb771aab674df2e6fcdb0ce8dc06ac879259a
kamiy2743 <kamiy2743@gmail.com>
2022-05-25 09:50:31 +0900

    フェーダー作成

---
+++ Assets/MyAssets/Fader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace MT.Fader
+{
+    public class Fader : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _ui;
+
+        public static Fader Instance => _instance;
+        private static Fader _instance;
+
+        void Awake()
+        {
+            _instance = this;
+        }
+
+        public void FadeIn(float duration, float endValue = 0, System.Action completed = null)
+        {
+            _ui.DOKill();
+            _ui.DOFade(endValue, duration).OnComplete(() => completed?.Invoke());
+        }
+
+        public void FadeOut(float duration, float endValue = 1, System.Action completed = null)
+        {
+            _ui.DOKill();
+            _ui.DOFade(endValue, duration).OnComplete(() => completed?.Invoke());
+        }
+    }
+}
