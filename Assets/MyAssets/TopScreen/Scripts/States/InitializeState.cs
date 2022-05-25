de47b9669ea5d7c6ad8dfc7bcd0c4dfdaaf7ca35
kamiy2743 <kamiy2743@gmail.com>
2022-05-25 13:54:39 +0900

    トップ画面初期化ステート

---
+++ Assets/MyAssets/TopScreen/Scripts/States/InitializeState.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MT.Util;
+using MT.Blocks;
+
+namespace MT.TopScreen.States
+{
+    public class InitializeState : MonoBehaviour, IState
+    {
+        [SerializeField] private GameObject _nextStateObject;
+
+        private IState _nextState;
+
+        void Start()
+        {
+            _nextState = _nextStateObject.GetComponent<IState>();
+            gameObject.SetActive(false);
+            Enter();
+        }
+
+        public void Enter()
+        {
+            gameObject.SetActive(true);
+            Initialize();
+            ToNext();
+        }
+
+        public void ToNext()
+        {
+            gameObject.SetActive(false);
+            _nextState.Enter();
+        }
+
+        private void Initialize()
+        {
+
+        }
+    }
+}
+
