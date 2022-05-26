3627e0dda4ea5f95a53ec870338b0441aff146cf
kamiy2743 <kamiy2743@gmail.com>
2022-05-26 21:12:25 +0900

    アプリケーション起動時初期化とかを書くクラスを作成

---
+++ Assets/MyAssets/ApplicationEntryPoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MT
+{
+    public class ApplicationEntryPoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject _topScreenObject;
+        [SerializeField] private MT.TopScreen.States.EnterState _topScreenEnterState;
+        [SerializeField] private GameObject _playScreenObject;
+        [SerializeField] private MT.PlayScreen.States.EnterState _playScreenEnterState;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (_topScreenObject.activeSelf)
+            {
+                _topScreenEnterState.Enter();
+                return;
+            }
+
+            if (_playScreenObject.activeSelf)
+            {
+                _playScreenEnterState.Enter();
+                return;
+            }
+        }
+    }
+}
