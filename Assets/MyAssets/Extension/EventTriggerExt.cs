496673bd98142044868d8a336c67c07cb429e561
kamiy2743 <kamiy2743@gmail.com>
2022-05-30 15:24:53 +0900

    System.ActionからUnityActionに変更

--- Assets/MyAssets/Extension/EventTriggerExt.cs
+++ Assets/MyAssets/Extension/EventTriggerExt.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 namespace Extension
 {
     public static class EventTriggerExt
     {
-        public static void AddListener(this EventTrigger eventTrigger, EventTriggerType eventTriggerType, System.Action callback)
+        public static void AddListener(this EventTrigger eventTrigger, EventTriggerType eventTriggerType, UnityAction call)
         {
             EventTrigger.Entry entry = new EventTrigger.Entry();
             entry.eventID = eventTriggerType;
-            entry.callback.AddListener((eventDate) => { callback.Invoke(); });
+            entry.callback.AddListener((eventDate) => call.Invoke());
             eventTrigger.triggers.Add(entry);
         }
     }
