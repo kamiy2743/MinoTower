a4b5f19d1dd6f3419866bf4f4c1bc211d857aea4
kamiy2743 <kamiy2743@gmail.com>
2022-05-31 12:09:38 +0900

    Iscreenから状態変数を削除

--- Assets/MyAssets/Screens/IScreen.cs
+++ Assets/MyAssets/Screens/IScreen.cs
@@ -3,8 +3,6 @@ namespace MT.Screens
     public interface IScreen
     {
         ScreenType Type { get; }
-        bool IsOpened { get; }
-        bool IsActive { get; }
         void Open();
         void Close();
     }
