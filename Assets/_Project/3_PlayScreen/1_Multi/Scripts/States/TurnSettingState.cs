611572cb49a0a5807e1b3eaae4a8602a843b3e01
kamiy2743 <kamiy2743@gmail.com>
2022-06-05 00:04:46 +0900

    自分のターンかどうかで何か処理をするステート

---
+++ Assets/_Project/3_PlayScreen/1_Multi/Scripts/States/TurnSettingState.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MT.PlayScreen.Multi
+{
+    public class TurnSettingState : MonoBehaviour, IState, IStaticAwake
+    {
+        [SerializeField] private PlayerTurnProvider _playerTurnProvider;
+        [SerializeField] private BlockSynchronizer _blockSynchronizer;
+
+        [Space(20)]
+        [SerializeField] private GameObject _nextStateObject;
+
+        private IState _nextState;
+
+        public void StaticAwake()
+        {
+            _nextState = _nextStateObject.GetComponent<IState>();
+        }
+
+        public void Enter()
+        {
+            if (_playerTurnProvider.IsMyTurn())
+            {
+
+            }
+            else
+            {
+
+            }
+
+            ToNext();
+        }
+
+        private void ToNext()
+        {
+            _nextState.Enter();
+        }
+    }
+}
