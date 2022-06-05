54cda6bb2159386428a38faccd00d7b64d721de2
kamiy2743 <kamiy2743@gmail.com>
2022-06-05 22:12:29 +0900

    CustomProperty反映待機を個別に行う、リファクタしろ

--- Assets/_Project/3_PlayScreen/1_Multi/Scripts/Systems/Network/CustomPropertyAccessor.cs
+++ Assets/_Project/3_PlayScreen/1_Multi/Scripts/Systems/Network/CustomPropertyAccessor.cs
@@ -2,29 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UniRx;
+using Cysharp.Threading.Tasks;
 
 namespace MT.PlayScreen.Multi
 {
-    public class CustomPropertyAccessor : MonoBehaviour, IStaticAwake
+    public class CustomPropertyAccessor : MonoBehaviourPunCallbacks, IStaticAwake
     {
         public static CustomPropertyAccessor Instance => _instance;
         private static CustomPropertyAccessor _instance;
+
+        private string _separator = ":separate:";
+        private Dictionary<string, bool> _dic = new Dictionary<string, bool>();
 
         public void StaticAwake()
         {
             _instance = this;
         }
 
-        public void Set<T>(string key, T value)
+        public async UniTask Set<T>(string key, T value)
         {
             if (!PhotonNetwork.InRoom)
             {
                 Debug.LogError("not in room");
             }
 
+            var time = System.DateTime.Now.Ticks;
+            var id = time + key + _separator + value;
+
             var cp = PhotonNetwork.CurrentRoom.CustomProperties;
-            cp[key] = value;
+            cp[key] = id;
             PhotonNetwork.CurrentRoom.SetCustomProperties(cp);
+
+            _dic[id] = false;
+            await UniTask.WaitUntil(() => _dic[id]);
+            _dic.Remove(id);
         }
 
         public T Get<T>(string key)
@@ -34,8 +46,24 @@ namespace MT.PlayScreen.Multi
                 Debug.LogError("not in room");
             }
 
-            var value = PhotonNetwork.CurrentRoom.CustomProperties[key];
-            return (T)value;
+            string value = PhotonNetwork.CurrentRoom.CustomProperties[key].ToString().Split(_separator)[1];
+            return (T)System.Convert.ChangeType((object)value, typeof(T));
+        }
+
+        // TODO えぐい
+        public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+        {
+            foreach (var value in propertiesThatChanged.Values)
+            {
+                foreach (var key in _dic.Keys)
+                {
+                    if (key == value.ToString())
+                    {
+                        _dic[key] = true;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
