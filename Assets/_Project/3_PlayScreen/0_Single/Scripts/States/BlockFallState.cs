b86aa6e39eacee8b3750b55cbfc2d682ad66847f
kamiy2743 <kamiy2743@gmail.com>
2022-06-05 15:31:31 +0900

    落下ステート

--- Assets/_Project/3_PlayScreen/0_Single/Scripts/States/BlockFallState.cs
+++ Assets/_Project/3_PlayScreen/0_Single/Scripts/States/BlockFallState.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 
-namespace MT.PlayScreen
+namespace MT.PlayScreen.Single
 {
     public class BlockFallState : MonoBehaviour, IState, IStaticAwake
     {
