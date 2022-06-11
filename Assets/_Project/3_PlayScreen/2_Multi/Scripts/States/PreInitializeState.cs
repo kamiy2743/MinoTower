c4a3fd8b051e5e1cf82faa419ac6e2a74c288539
kamiy2743 <kamiy2743@gmail.com>
2022-06-11 14:16:51 +0900

    マルチ画面の初期化処理

---
+++ Assets/_Project/3_PlayScreen/2_Multi/Scripts/States/PreInitializeState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Photon.Pun;
+
+namespace MT.PlayScreen.Multi
+{
+    public class PreInitializeState : MonoBehaviourPunCallbacks, IPreInitializeStateAsync
+    {
+        [Header("初期化対象")]
+        [SerializeField] private RandomProvider _randomProvider;
+        [SerializeField] private BlockStore _blockStore;
+        [SerializeField] private RotateButton _rotateButton;
+        [SerializeField] private ScreenScroller _screenScroller;
+        [SerializeField] private ResultUI _resultUI;
+
+        private PlayerTurnProvider _playerTurnProvider = new PlayerTurnProvider();
+
+        public async UniTask Enter()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                var seed = (int)System.DateTime.Now.Ticks;
+                photonView.RPC(nameof(RandomInitialize), RpcTarget.All, seed);
+            }
+
+            PhotonNetwork.SendRate = 60;
+            PhotonNetwork.SerializationRate = 60;
+
+            _blockStore.Initialize();
+            _resultUI.HideAsync(0).Forget();
+            _screenScroller.Initialize();
+            _rotateButton.HideAsync(0).Forget();
+            await _playerTurnProvider.InitializeAsync();
+        }
+
+        [PunRPC]
+        private void RandomInitialize(int seed)
+        {
+            _randomProvider.RandomForBlock = new CustomRandom(seed);
+        }
+    }
+}
